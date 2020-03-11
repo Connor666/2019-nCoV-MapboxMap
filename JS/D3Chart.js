89128fe8602ec2a43df2338a032c10b75ca73a9b
Connor666 <809068445@qq.com>
2020-03-11 15:47:19 +0000

    add autoplay

--- JS/D3Chart.js
+++ JS/D3Chart.js
@@ -1,37 +1,32 @@
-function JSONObject(type,Date,People){
-    this.type=type;
-    this.Date=Date;
-    this.People=People;
+// This JS file is to set D3 chart
+function JSONObject(type, Date, People) { // Function to make jsonobject
+    this.type = type;
+    this.Date = Date;
+    this.People = People;
 }
 
 
-function chartSet(index){
-    d3.csv("data.csv", function(CovData) {
+function chartSet(index) { // Function to generate D3 chart
+    d3.csv("data.csv", function (CovData) {
 
-        var DataIndex = index-1;
-        console.log(CovData);
-        document.getElementById("charttitle").innerHTML = CovData[DataIndex].province; // The name of the city and country is inserted into the chart title
-        console.log(document.getElementById("chartContainer"))
+        var DataIndex = index - 1; // Find index
+        document.getElementById("charttitle").innerHTML = CovData[DataIndex].province; // The name of the city is inserted into the chart title
         var svg = dimple.newSvg("#chartContainer", 500, 250); // The chart is an svg variable assigned to the chartcontainer div. It's width and height are also assigned here
-        var chartdata =[];
-        for (var key in CovData[DataIndex]){
-            if (key.match(RegExp(/confirm/))){
-                chartdata.push(new JSONObject("Confirmed",key.replace("_confirm",""),CovData[DataIndex][key]))
-            }
-            else if (key.match(RegExp(/heal/))){
-                chartdata.push(new JSONObject("Recovered",key.replace("_heal",""),CovData[DataIndex][key]))
-            }
-
-            else if(key.match(RegExp(/dead/))){
-                chartdata.push(new JSONObject("Deaths",key.replace("_dead",""),CovData[DataIndex][key]))
+        var chartdata = [];
+        for (var key in CovData[DataIndex]) {  // Data into different group according to the type
+            if (key.match(RegExp(/confirm/))) {
+                chartdata.push(new JSONObject("Confirmed", key.replace("_confirm", ""), CovData[DataIndex][key]))
+            } else if (key.match(RegExp(/heal/))) {
+                chartdata.push(new JSONObject("Recovered", key.replace("_heal", ""), CovData[DataIndex][key]))
+            } else if (key.match(RegExp(/dead/))) {
+                chartdata.push(new JSONObject("Deaths", key.replace("_dead", ""), CovData[DataIndex][key]))
             }
         }
 
-        console.log(chartdata)
         var myChart = new dimple.chart(svg, chartdata);
         myChart.setBounds(60, 15, 390, 200);            // Set the chart bounds within the svg container
 
-        myChart.defaultColors = [
+        myChart.defaultColors = [ // Set color
             new dimple.color("#fc4e2a"),
             new dimple.color("#31a354"),
             new dimple.color("#3182bd")
@@ -39,7 +34,7 @@ function chartSet(index){
 
         myChart.addLegend(100, 0, 380, 200, "right");
 
-        var x = myChart.addTimeAxis("x", "Date","%m-%d","%m-%d");  // Define the x axis. The latter statements define the date format which we want to be year only
+        var x = myChart.addTimeAxis("x", "Date", "%m-%d", "%m-%d");  // Define the x axis and the format
         x.timeInterval = 1;
 
 
@@ -52,7 +47,7 @@ function chartSet(index){
         myChart.draw(1600);
 
 
-        svg.selectAll("path.dimple-proj").style("stroke", "2"); // Some minor stying changes using the svg selectAll statement. Make the projected data a dashed line and the grid colour lighter.
+        svg.selectAll("path.dimple-proj").style("stroke", "2");// some style change
         svg.selectAll("path.domain").style("stroke", "#CCC");
         svg.selectAll("g.tick line").style("stroke", "#CCC");
 
